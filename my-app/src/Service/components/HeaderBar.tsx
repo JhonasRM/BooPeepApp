7900c2497867233b97e679fce63e88e16fc7fe0c
MaykeBolt <mayke.silva6@etec.sp.gov.br>
2024-03-16 02:19:39 -0300

    fix:trocando o icone do HeaderBar e da SplashScreen.

--- my-app/src/Service/components/HeaderBar.tsx
+++ my-app/src/Service/components/HeaderBar.tsx
@@ -7,7 +7,7 @@ import SearchBar from "./SearchBar";
 const HeaderBar = () => {
     return (
         <View style={styles.header} dataSet={{media: ids.header}}>
-            <Image source={require('../../../assets/icons/BooPeep2.jpg')} style={styles.icon} />
+            <Image source={require('../../../assets/icons/2-removebg-preview.png')} style={styles.icon} />
             <SearchBar />
             {/* <Pressable style={styles.button} dataSet={{media: ids.button}}><Image source={require('../../../assets/icons/icons8-pesquisar-64.png')} style={styles.img} /></Pressable> */}
         </View>
@@ -28,12 +28,11 @@ const {styles} = StyleSheet.create ({
         
     },
     icon: {
-        
-        width: 140,
-        height: 50,
+        width: 130,
+        height: 55,
         marginLeft: wp(2),
         marginTop: hp(2),
-        marginBottom: hp(2),
+        marginBottom: hp(2)
     }
 })
 
