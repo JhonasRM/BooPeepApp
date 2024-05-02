f7db23d6e7e42b7c83453875e249e1298ef7397d
MaykeBolt <mayke.silva6@etec.sp.gov.br>
2024-05-02 14:23:52 -0300

    feat: Fazendo o botão de Opções do Post virar um Modal.

--- my-app/src/app/Presentation/View/components/ContainerOptions.tsx
+++ my-app/src/app/Presentation/View/components/ContainerOptions.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { useState } from "react"
-import { Image, StyleProp, StyleSheet, Text, TouchableOpacity, View, ViewStyle } from "react-native"
+import { Image, StyleProp, StyleSheet, Text, TouchableOpacity, TouchableWithoutFeedback, View, ViewStyle, Modal, Pressable } from "react-native"
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from "react-native-responsive-screen";
 
 interface Props {
     style?: StyleProp<ViewStyle> //"Isso será usado para receber o estilo do Parent!" - Bolt
@@ -10,24 +11,46 @@ const ContainerOptions = (props: Props) => {
     const [isPress, setIsPress] = useState(false)
 
     const handlePress = () => {
-        setIsPress(isPress => !isPress)
+        setIsPress(true)
     }
+
+   const handleExit = () => {
+	setIsPress(false)
+   }
+
     //if/else based on click boolean
     //Will open a menu and hide the icon image
 
     return (
+        <>
+        { isPress == false ? (
         <TouchableOpacity style={props.style} onPress={handlePress}>
-            { isPress == false ? (
             <Image source={require('../../../../../assets/icons/icons8-menu-2-24.png')}/>
-            ) : (
-            <View style={styles.optionsmenu}>
-                    <TouchableOpacity style={styles.optioncontainer}>
-                        <Image source={require('../../../../../assets/icons/icons8-menu-2-24.png')} style={styles.btnImg}/>
-                        <Text style={[styles.btnText, {color: "red"}]}>Denunciar Post</Text>
-                    </TouchableOpacity>
-            </View>
-            )}
         </TouchableOpacity>
+        ) : (
+        <Modal onRequestClose={handleExit} animationType="slide" transparent={true}>
+            <TouchableOpacity onPress={handleExit} style={styles.outsideModal} activeOpacity={0}>
+                <Text />
+            </TouchableOpacity>
+
+            <View style={styles.modal}>
+                <View style={styles.headerBar}>
+                    <TouchableOpacity onPress={handleExit} style={styles.exitBtn}>
+                        <Image source={require("../../../../../assets/icons/icons8-fechar-janela-100.png")} 
+                        style={styles.exitBtnImg}/>
+                    </TouchableOpacity>
+
+                    <Text style={styles.headerText}>Opções da Postagem</Text>
+                </View>
+                
+                <TouchableOpacity style={styles.optioncontainer}>
+                    <Image source={require('../../../../../assets/icons/icons8-menu-2-24.png')} style={styles.btnImg}/>
+                    <Text style={[styles.btnText, {color: "red"}]}>Denunciar Post</Text>
+                </TouchableOpacity>
+            </View>
+        </Modal>
+        )}
+        </>
     )
 }
 
@@ -39,18 +62,24 @@ const styles = StyleSheet.create({
         backgroundColor: "#d4d7ff",
         paddingVertical: 2,
     },
+    modal: {
+        backgroundColor: "#FFF", 
+        position: "absolute", 
+        bottom: 0, left: 0, right: 0, top: hp(50),
+    },
+    outsideModal: {   
+        backgroundColor: "#00000080",
+        position: "absolute",
+        top: 0, bottom: 0, left: 0, right: 0
+    },
     optioncontainer: {
-        backgroundColor: "#ffffff",
-        borderColor: "#000000",
-        borderWidth: 1.25,
+        paddingVertical: 14,
+        paddingLeft: 10,
 
         flexDirection: "row",
         alignItems: "center",
-        paddingHorizontal: 4,
-        paddingVertical: 4,
-        marginVertical: 2,
-        paddingRight: 10,
-        marginHorizontal: 4,
+        borderBottomColor: "#888888",
+        borderBottomWidth: 2,
     },
     btnImg: {
         
@@ -58,6 +87,29 @@ const styles = StyleSheet.create({
     btnText: {
         fontWeight: "bold"
     },
+
+    //Header Bar
+    headerBar: {
+        backgroundColor: "#d4d7ff",
+        borderBottomColor: "#888888",
+        borderBottomWidth: 2,
+        flexDirection: "row",
+        alignItems: "center",
+        paddingVertical: 10,
+        paddingLeft: 5,
+    },
+    exitBtn: {
+
+    },
+    exitBtnImg: {
+        width: 40,
+        height: 40,
+    },
+    headerText: {
+        marginLeft: wp(2),
+        fontSize: 19,
+        fontWeight: "bold"
+    }
 })
 
 export default ContainerOptions
