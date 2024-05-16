a9ed38584b87044066e5354f13ba50e98facae23
IgorRM15 <i.rocha.martins10@gmail.com>
2024-05-15 22:31:10 -0300

    add/ pastas singup/singin

---
+++ my-app/src/app/Service/Storage/SingInUser.ts
@@ -0,0 +1,4 @@
+import React, { useMemo, useState } from 'react';
+import { Alert, Keyboard } from 'react-native';
+import { useNavigation } from '@react-navigation/core';
+import AsyncStorage from '@react-native-async-storage/async-storage';
