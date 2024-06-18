7b76098495ec381e6faea4d3c492e92e876839ca
MaykeBolt <136872494+MaykeBolt@users.noreply.github.com>
2024-06-18 20:00:13 +0000

    feat: Configurando o containerOptionsStateController (Falta o parametro).

--- my-app/src/app/Presentation/Controllers/containerOptionsStateController.ts
+++ my-app/src/app/Presentation/Controllers/containerOptionsStateController.ts
@@ -1,5 +1,29 @@
-const containerOptionsStateController = () => {
+import { postRepository } from "../../Data Access/Repository/postRepository"
+import { IReturnAdapter } from "../../utils/Interfaces/IReturnAdapter"
 
+const containerOptionsStateController = () => {
+    const postRep = new postRepository()
+
+    const DeletePost = async(): Promise<IReturnAdapter> => {
+        try {
+            const deletePost = await postRep.deletePost("0")
+
+            if (deletePost.val === false) {
+                throw new Error(deletePost.erro)
+            }
+
+            return {val: true, erro: deletePost.data}
+        } catch (error) {
+            if(error instanceof Error){
+                return {val: false, erro: error.message}
+            }
+            return{val: false, erro: `Erro interno da aplicação. Tente novamente mais tarde`}
+        }
+    }
+
+    return {
+        DeletePost
+    }
 }
 
 export { containerOptionsStateController }
